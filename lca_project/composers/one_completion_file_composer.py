067663b32981fa4c646ef34a8b58aaaffcac303e
Natalie Neamtu <student34@login1.hpcfund>
2024-12-09 17:58:26 -0600

    first attempt at context-window-aware cropping

--- lca_project/composers/one_completion_file_composer.py
+++ lca_project/composers/one_completion_file_composer.py
@@ -11,3 +11,34 @@ class OneCompletionFileComposer(ComposerBase):
         assert len(completion) == 1, 'Only one file should be completed'
         content = list(completion.values())[0]
         return content
+    
+    def repo_metainfo(self, repo_name: str) -> str:
+        return f"{self.extension}{self.lang_sep_symbol}{repo_name}{self.meta_info_sep_symbol}"
+    
+    def compose_with_context_truncation(self, composed_content: list, datapoint: DatapointBase) -> str:
+        completion = datapoint.get_completion()
+        assert len(completion) == 1, 'Only one file should be completed'
+        completion_path = list(completion)[0]
+
+        repo_metainfo = f"{self.extension}{self.lang_sep_symbol}{datapoint.repo_name}{self.meta_info_sep_symbol}"
+
+        # truncate composed_context until everything will fit in seq_max_len
+        boilerplate_len = len(repo_metainfo) + len(completion_path) + len(self.completion_composer(datapoint))
+        composed_content_lens = [len(s) for s in composed_content]
+        # estimate: 2.5 characters -> 1 token
+        max_len = int(self.seq_max_len * 2.5)
+        i = len(composed_content)
+        total = sum(composed_content_lens) + boilerplate_len
+        original = total
+        while i >= 0 and total > max_len:
+            i -= 1
+            total -= composed_content_lens[i]
+
+        composed_content = composed_content[:i]
+        composed_content.append(completion_path + self.meta_info_sep_symbol)
+
+        final = repo_metainfo + self.lang_sep_symbol.join(composed_content)
+
+        #print(str(original) + ', ' + str(len(final)))
+
+        return final
--- lca_project/composers/random_composer.py
+++ lca_project/composers/random_composer.py
@@ -7,16 +7,8 @@ class RandomComposer(OneCompletionFileComposer):
     def context_composer(self, datapoint: DatapointBase) -> str:
         # context = datapoint['context']
         context = datapoint.get_context()
-        repo_name = datapoint.repo_name
         keys = list(context.keys())
         random.shuffle(keys)
         composed_content = [path + self.meta_info_sep_symbol + context[path] for path in keys]
 
-        completion = datapoint.get_completion()
-        assert len(completion) == 1, 'Only one file should be completed'
-        completion_path = list(completion)[0]
-        composed_content.append(completion_path + self.meta_info_sep_symbol)
-
-        repo_metainfo = f"{self.extension}{self.lang_sep_symbol}{repo_name}{self.meta_info_sep_symbol}"
-
-        return repo_metainfo + self.lang_sep_symbol.join(composed_content)
+        return super().compose_with_context_truncation(composed_content, datapoint)
